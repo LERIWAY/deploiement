43399afa3c98da51d8e1c797553ed5d273a1e657
Okkes Amoca <okkes2001@hotmail.fr>
2022-11-23 22:39:42 +0100

    login

--- deploy/api/index.php
+++ deploy/api/index.php
@@ -10,13 +10,63 @@ const JWT_SECRET = "makey1234567";
 
 $app = AppFactory::create();
 
-
 $app->get('/api/hello/{name}', function (Request $request, Response $response, $args) {
     $array = [];
     $array ["nom"] = $args ['name'];
     $response->getBody()->write(json_encode ($array));
     return $response;
 });
+
+// APi d'authentification générant un JWT
+$app->post('/api/login', function (Request $request, Response $response, $args) {   
+    $err=false;
+    $body = $request->getParsedBody();
+    $login = $body ['login'] ?? "";
+    $pass = $body ['pass'] ?? "";
+
+    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$login))   {
+        $err = true;
+    }
+    if (!preg_match("/[a-zA-Z0-9]{1,20}/",$pass))  {
+        $err=true;
+    }
+
+    if (!$err) {
+            $response = createJwT ($response);
+            $data = array('nom' => 'toto', 'prenom' => 'titi');
+            $response->getBody()->write(json_encode($data));
+     } else {          
+            $response = $response->withStatus(401);
+     }
+    return $response;
+});
+
+// get user 
+$app->get('/api/user', function (Request $request, Response $response, $args) {
+    $array = [];
+    $array ["nom"] = "AMOCA";
+    $array ["prenom"] = "Okkes";
+    $response = addHeaders($response);
+    $response->getBody()->write(json_encode ($array));
+    return $response;
+});
+
+function createJwT (Response $response) : Response {
+
+    $issuedAt = time();
+    $expirationTime = $issuedAt + 6000;
+    $payload = array(
+    'userid' => '1',
+    'email' => 'okkes2001@hotmail.fr',
+    'pseudo' => 'leriway',
+    'iat' => $issuedAt,
+    'exp' => $expirationTime
+    );
+
+    $token_jwt = JWT::encode($payload,JWT_SECRET, "HS256");
+    $response = $response->withHeader("Authorization", "Bearer {$token_jwt}");
+    return $response;
+}
 
 $options = [
     "attribute" => "token",
@@ -26,13 +76,20 @@ $options = [
     "algorithm" => ["HS256"],
     "secret" => JWT_SECRET,
     "path" => ["/api"],
-    "ignore" => ["/api/hello"],
+    "ignore" => ["/api/hello","/api/login","/api/createUser"],
     "error" => function ($response, $arguments) {
         $data = array('ERREUR' => 'Connexion', 'ERREUR' => 'JWT Non valide');
         $response = $response->withStatus(401);
         return $response->withHeader("Content-Type", "application/json")->getBody()->write(json_encode($data));
     }
 ];
+
+$app->get('/api/product', function (Request $request, Response $response, $args) {
+    $json = file_get_contents("./mock/catalogue.json");
+    $response = addHeaders($response);
+    $response->getBody()->write($json);
+    return $response;
+});
 
 $app->add(new Tuupola\Middleware\JwtAuthentication($options));
 $app->run ();
